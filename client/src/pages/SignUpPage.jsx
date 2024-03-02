0cf2f26ccef96b3ea0758818bdd6be45baa30195
Monkey D. Luffy <markolekic124@gmail.com>
2024-03-02 21:53:35 +0100

    signuppage ui

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,6 +1,48 @@
+import { Button, Label, TextInput } from "flowbite-react"
+import { Link } from "react-router-dom"
+
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="border border-slate-300 rounded-lg w-[350px] max-lg:w-[280px] p-4">
+        <div className="text-2xl text-center">Sign Up</div>
+        <form className="flex flex-col gap-4">
+          <div>
+            <div className="mb-1">
+              <Label className="text-lg font-medium">Username</Label>
+            </div>
+            <TextInput 
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div>
+            <div className="mb-1">
+              <Label className="text-lg font-medium">Email Address</Label>
+            </div>
+            <TextInput 
+              type="text"
+              placeholder="Email Address"
+            />
+          </div>
+          <div>
+            <div className="mb-1">
+              <Label className="text-lg font-medium">Password</Label>
+            </div>
+            <TextInput 
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <Button gradientDuoTone={"purpleToPink"}>
+            Sign Up
+          </Button>
+        </form>
+        <div className="hover:text-pink-500 cursor-pointer hover:underline mt-2">
+          <Link to={"/sign-in"}>Already have an account? Sign In</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
